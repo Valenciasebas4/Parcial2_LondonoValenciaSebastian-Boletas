7ae9ab265390d8e1d5283c44365affdcc5ca487a
SEBASTIAN LONDO�O VALENCIA <sebastianlondono264947@correo.itm.edu.co>
2023-04-15 11:09:35 -0500

    Create new folder DAL and new folder Entities, too create class DataBaseContext. create Builder in class program for the conection database

---
+++ Parcial2_LondonoValenciaSebastian/Parcial2_LondonoValenciaSebastian/DAL/DataBaseContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial2_LondonoValenciaSebastian.DAL.Entities;
+using System.Diagnostics.Metrics;
+
+namespace Parcial2_LondonoValenciaSebastian.DAL
+{
+    public class DataBaseContext :DbContext
+    {
+
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+
+        }
+        // Se mapea la identidad para convertirla en una tabla
+        public DbSet<Ticket> Tickets { get; set; }
+        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Ticket>().HasIndex(c => c.Id).IsUnique();
+            
+        }
+    }
+}
---
+++ Parcial2_LondonoValenciaSebastian/Parcial2_LondonoValenciaSebastian/DAL/Entities/Ticket.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parcial2_LondonoValenciaSebastian.DAL.Entities
+{
+    public class Ticket
+    {
+        #region Properties
+        [Key] //PROPERTY KEY
+        public virtual Guid Id { get; set; }
+
+        [Display(Name = "Fecha de Uso")]
+        public virtual DateTime? UseDate { get; set; }
+
+        [Display(Name = "Se uso?")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe ser de {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es oblilgatorio.")]
+        public string IsUsed { get; set; }
+
+        [Display(Name = "Porteria de Entrada")]
+        [MaxLength(500, ErrorMessage = "El campo {0} debe ser de {1} caracteres.")]
+        public string? EntranceGate { get; set; }
+        #endregion
+    }
+}
