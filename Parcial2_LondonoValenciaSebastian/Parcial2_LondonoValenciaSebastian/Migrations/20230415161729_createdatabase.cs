e4642a943751cafeeabe879e894e169d299ef53a
SEBASTIAN LONDO�O VALENCIA <sebastianlondono264947@correo.itm.edu.co>
2023-04-15 11:20:37 -0500

    Create Controllers Entity Ticket and create database ConcertBD

---
+++ Parcial2_LondonoValenciaSebastian/Parcial2_LondonoValenciaSebastian/Migrations/20230415161729_createdatabase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Parcial2_LondonoValenciaSebastian.Migrations
+{
+    /// <inheritdoc />
+    public partial class createdatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tickets",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UseDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsUsed = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    EntranceGate = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tickets", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_Id",
+                table: "Tickets",
+                column: "Id",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tickets");
+        }
+    }
+}
